8d0b05946d7ad86383bec793983c78230c48afb6
Muahmmed Favas <favasfvs111@gmail.com>
2024-05-30 23:45:40 -0700

    page for listing todos and marking as completed and removing

---
+++ src/Components/ToDoList.jsx
@@ -0,0 +1,28 @@
+
+function ToDoList({todo,removeTodo}){
+
+    return(
+        <div className="w-full">
+            <table>
+                <thead>
+                    <tr>
+                        <th>ToDo</th>
+                        <th>Status</th>
+                        <th>Remove</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                {todo.map(todos=>(
+                    <tr key={todos.id}>
+                        <td>{todos.task}</td>
+                        <td><button>Completed</button></td>
+                        <td><button onClick={()=>removeTodo(todos.id)}>Remove</button></td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+export default ToDoList
